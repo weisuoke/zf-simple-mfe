ee00dba4287f14f76a845b43caf14e91a8599748
梅林的法杖 <530264127@qq.com>
2020-06-27 14:55:41 +0800

    init-project

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.export = function(api) {
+  // 缓存babel的配置
+  api.cache(true);  // 等同于api.cache.forever();
+
+  return {
+    presets: [
+      ['@babel/preset', {module: false}]
+    ],
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import'
+    ]
+  }
+}
